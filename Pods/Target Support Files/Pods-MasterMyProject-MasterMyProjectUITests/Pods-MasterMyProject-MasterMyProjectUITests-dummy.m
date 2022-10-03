33fa0ed52d562935d58972496f9a86669e56e9d7
Nagesh Rangapure <nagesh.rangapure@kohler.com>
2022-10-03 20:20:23 +0530

    Set up project pod set up

---
+++ Pods/Target Support Files/Pods-MasterMyProject-MasterMyProjectUITests/Pods-MasterMyProject-MasterMyProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MasterMyProject_MasterMyProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MasterMyProject_MasterMyProjectUITests
+@end
