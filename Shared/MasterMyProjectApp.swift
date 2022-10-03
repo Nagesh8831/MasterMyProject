84184ae83c41561cd4c30d91784d2cf1420223b5
Nagesh Rangapure <nagesh.rangapure@kohler.com>
2022-10-03 20:02:28 +0530

    Initial commit

---
+++ Shared/MasterMyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterMyProjectApp.swift
+//  Shared
+//
+//  Created by KO158S8 on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MasterMyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
